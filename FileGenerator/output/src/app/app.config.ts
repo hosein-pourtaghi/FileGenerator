0d8b236674a5defb07fad792da025737845593dd
hosein pourtaghi <hosein.pourtaghi@gmail.com>
2026-05-06 12:02:10 +0330

    fix

--- FileGenerator/output/src/app/app.config.ts
+++
@@ -1,5 +0,0 @@
-﻿
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter, withComponentInputBinding } from '@angular/router';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { provideAnimations } from '@angular/platform-browser/animations';
--- FileGenerator/output/src/app/core/interceptors/loading.interceptor.ts
+++
@@ -1,7 +0,0 @@
-﻿
-import { HttpInterceptorFn } from '@angular/common/http';
-import { inject } from '@angular/core';
-import { LoadingService } from '../services/loading.service';
-
-export const loadingInterceptor: HttpInterceptorFn = (req, next) => {
-  const loadingService = inject(LoadingService);
